74310370c464e3996878677dad59e6c0deac5b06
SyedTalha71x <syedtalha71x@gmail.com>
2025-12-20 15:47:33 +0500

    Commit: update pipeline and helm charts

--- eks-terraform/modules/eks-cluster/main.tf
+++ eks-terraform/modules/eks-cluster/main.tf
@@ -1,8 +1,10 @@
+# =========================
+# EKS Cluster
+# =========================
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
   role_arn = aws_iam_role.cluster.arn
   version  = var.kubernetes_version
-
 
   bootstrap_self_managed_addons = false
 
@@ -41,10 +43,9 @@ resource "aws_eks_cluster" "main" {
   ] : []
 
   access_config {
-  authentication_mode                         = "API_AND_CONFIG_MAP"
-  bootstrap_cluster_creator_admin_permissions = true
-  # bootstrap_self_managed_addons               = false 
-}
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
 
   encryption_config {
     provider {
@@ -57,5 +58,49 @@ resource "aws_eks_cluster" "main" {
 
   depends_on = [
     aws_iam_role_policy_attachment.cluster_policies
+  ]
+}
+
+# =========================
+# Nodegroup: System
+# =========================
+resource "aws_eks_node_group" "system" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "system"
+  node_role_arn   = aws_iam_role.cluster.arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = 2
+    min_size     = 1
+    max_size     = 3
+  }
+
+  instance_types = ["t3.small"]
+
+  depends_on = [
+    aws_eks_cluster.main
+  ]
+}
+
+# =========================
+# Nodegroup: General Purpose
+# =========================
+resource "aws_eks_node_group" "general_purpose" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "general-purpose"
+  node_role_arn   = aws_iam_role.cluster.arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = 2
+    min_size     = 1
+    max_size     = 3
+  }
+
+  instance_types = ["t3.small"]
+
+  depends_on = [
+    aws_eks_cluster.main
   ]
 }
